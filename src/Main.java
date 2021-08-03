605521b59fadf52334c9e533ccdf843aa02eee3f
Alexey Dubeshko <dubeshko.alexei@gmail.com>
2021-08-03 17:32:14 +0300

    f

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import actions.GetInfo;
+import actions.Tasks;
+import javafx.concurrent.Task;
+
+public class Main {
+    public static void main(String[] args) {
+        int taskNumber = GetInfo.getInt("Please, enter the number of the task from 1 to 2: ");
+        switch (taskNumber){
+            case 1:
+                Tasks.task1();
+                break;
+            case 2:
+                Tasks.task2();
+                break;
+            default:
+                System.out.println("Incorrect task number!");
+        }
+    }
+}
---
+++ src/actions/Tasks.java
@@ -0,0 +1,13 @@
+package actions;
+
+public class Tasks {
+    public static void task1(){
+
+        int[][] matrix = Create.randomMatrix(5,100);
+        Output.matrix(matrix);
+
+    }
+    public static void task2(){
+
+    }
+}
