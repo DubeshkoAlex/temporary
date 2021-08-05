b507d9fab96dd55d1aa31bbe66bca3a418a3c360
Alexey Dubeshko <dubeshko.alexei@gmail.com>
2021-08-05 16:27:34 +0300

    third commit

--- src/actions/OperationWithMatrix.java
+++ src/actions/OperationWithMatrix.java
@@ -2,61 +2,55 @@ package actions;
 
 public class OperationWithMatrix {
 
+    public static void sortMatrix(int[][] arr, int k, boolean columnSort) {
+
+        // if true sort by column else sort my row
+        int from = 0;
+        int to = arr.length - 1;
+
+        if(columnSort){
+            quickSort(arr,from,to,k);
+        }
+        else{
+            reverseMatrix(arr);
+            quickSort(arr,from,to,k);
+            reverseMatrix(arr);
+        }
+    }
+
     public static void quickSort(int[][] arr, int from, int to, int k){
 
-        if(from < to){
-            int divideIndex = partition(arr, from, to,k);
-            quickSort(arr,from,divideIndex - 1,k);
-            quickSort(arr, divideIndex, to,k);
-        }
+            if(from < to){
+                int divideIndex = partition(arr, from, to,k);
+                quickSort(arr,from,divideIndex - 1,k);
+                quickSort(arr, divideIndex, to,k);
+            }
+
     }
 
-//    private static int partition(int[][] arr, int from, int to, int k){
-//        int rightIndex = to;
-//        int leftIndex = from;
-//
-//        int pivot = arr[(from+to)/2][k];
-//        while(leftIndex <= rightIndex){
-//
-//            while (arr[leftIndex][k] < pivot){
-//                leftIndex++;
-//            }
-//
-//            while (arr[rightIndex][k] > pivot){
-//                rightIndex--;
-//            }
-//
-//            if(leftIndex <= rightIndex){
-//                swap(arr, rightIndex, leftIndex);
-//                leftIndex++;
-//                rightIndex--;
-//            }
-//        }
-//        return leftIndex;
-//    }
-private static int partition(int[][] arr, int from, int to, int k){
-    int rightIndex = to;
-    int leftIndex = from;
+    private static int partition(int[][] arr, int from, int to, int k){
+        int rightIndex = to;
+        int leftIndex = from;
 
-    int pivot = arr[k][(from+to)/2];
-    while(leftIndex <= rightIndex){
+        int pivot = arr[(from+to)/2][k];
+        while(leftIndex <= rightIndex){
 
-        while (arr[k][leftIndex] < pivot){
-            leftIndex++;
+            while (arr[leftIndex][k] < pivot){
+                leftIndex++;
+            }
+
+            while (arr[rightIndex][k] > pivot){
+                rightIndex--;
+            }
+
+            if(leftIndex <= rightIndex){
+                swap(arr, rightIndex, leftIndex);
+                leftIndex++;
+                rightIndex--;
+            }
         }
-
-        while (arr[k][rightIndex] > pivot){
-            rightIndex--;
-        }
-
-        if(leftIndex <= rightIndex){
-            swap(arr, rightIndex, leftIndex);
-            leftIndex++;
-            rightIndex--;
-        }
+        return leftIndex;
     }
-    return leftIndex;
-}
 
     public static void swap(int[][] array, int index1, int index2){
        arraySwap(array[index1],array[index2]);
@@ -66,13 +60,28 @@ private static int partition(int[][] arr, int from, int to, int k){
         int[] tmp = new int[array1.length];
         for (int i = 0; i < array1.length; i++){
             tmp[i] = array1[i];
-        }
-        for (int i = 0; i < array2.length; i++){
             array1[i] = array2[i];
-        }
-        for (int i = 0; i < tmp.length; i++){
             array2[i] = tmp[i];
         }
+    }
+
+    public static void reverseMatrix(int[][] array){
+
+        int[][] matrix = new int[array.length][array.length];
+        int k = 0;
+
+        for (int i = 0; i < array.length; i++,k++){
+            for (int j = 0; j < array.length; j++){
+                matrix[j][k] = array[i][j];
+            }
+        }
+
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array.length; j++){
+                array[i][j] = matrix[i][j];
+            }
+        }
+
     }
 
 }
--- src/actions/Tasks.java
+++ src/actions/Tasks.java
@@ -1,12 +1,16 @@
 package actions;
 
+import javax.sound.midi.Soundbank;
+
 public class Tasks {
     public static void task1(){
-
-        int[][] matrix = Create.randomMatrix(5,100);
+        int matrixSize = GetInfo.getInt("Please input the size of matrix: ");
+        boolean columnOrRowSort = GetInfo.getBoolean("Please input the type of sort: by column - true, by row - false: ");
+        int numberOfRowOrColumn = GetInfo.getInt("Please input the number of row or column from 1 to "+ matrixSize + ": ")-1;
+        int[][] matrix = Create.randomMatrix(matrixSize,100);
         Output.matrix(matrix);
         System.out.println("\n \n");
-        OperationWithMatrix.quickSort(matrix,0, matrix.length-1,0);
+        OperationWithMatrix.sortMatrix(matrix,numberOfRowOrColumn,columnOrRowSort);
         Output.matrix(matrix);
     }
     public static void task2(){
