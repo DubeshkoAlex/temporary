490fa56df65945e31b0405a2b8ce50a374ff69c5
Alexey Dubeshko <dubeshko.alexei@gmail.com>
2021-09-22 14:05:35 +0300

    second

--- src/utils/ReaderFile.java
+++ src/utils/ReaderFile.java
@@ -1,8 +1,12 @@
 package utils;
 
 
-import Cars.FamilyCar;
-import Cars.CarType;
+import Cars.*;
+import carClasses.BusinessClass;
+import carClasses.ComfortClass;
+import carClasses.EconomyClass;
+import carClasses.FamilyClass;
+
 import java.io.*;
 import java.nio.CharBuffer;
 import java.util.ArrayList;
@@ -13,6 +17,7 @@ public class ReaderFile {
 
     private static final String CARS_PATH = "C:/javaprojects/Collections_MainTask/src/Cars.txt";
     private List<String> listFile = new ArrayList<>();
+    public List<Car> fileCars = new ArrayList<>();;
 
     private void readFile() throws IOException {
         Reader file = new FileReader(CARS_PATH);
@@ -28,13 +33,75 @@ public class ReaderFile {
         readFile();
         String[] substring;
         String delimeter = ",";
+
         for (String s: listFile) {
             substring = s.split(delimeter);
-
-
-
-            System.out.println(FamilyCar.class.getSimpleName().toLowerCase(Locale.ROOT));
+            switch (CarType.valueOf(substring[0])){
+                case ECONOMY_CAR:
+                    fileCars.add(new EconomyCar(
+                            substring[1],
+                            substring[2],
+                            Integer.parseInt(substring[3]),
+                            Double.parseDouble(substring[4]),
+                            Double.parseDouble(substring[5]),
+                            Double.parseDouble(substring[6]),
+                            Double.parseDouble(substring[7]),
+                            Integer.parseInt(substring[8]),
+                            EconomyClass.valueOf(substring[9]))
+                    );
+                    break;
+                case FAMILY_CAR:
+                    fileCars.add(new FamilyCar(
+                            substring[1],
+                            substring[2],
+                            Integer.parseInt(substring[3]),
+                            Double.parseDouble(substring[4]),
+                            Double.parseDouble(substring[5]),
+                            Double.parseDouble(substring[6]),
+                            Double.parseDouble(substring[7]),
+                            Integer.parseInt(substring[8]),
+                            FamilyClass.valueOf(substring[9]),
+                            Integer.parseInt(substring[10]),
+                            Integer.parseInt(substring[11]))
+                    );
+                    break;
+                case COMFORT_CAR:
+                    fileCars.add(new ComfortCar(
+                            substring[1],
+                            substring[2],
+                            Integer.parseInt(substring[3]),
+                            Double.parseDouble(substring[4]),
+                            Double.parseDouble(substring[5]),
+                            Double.parseDouble(substring[6]),
+                            Double.parseDouble(substring[7]),
+                            Integer.parseInt(substring[8]),
+                            ComfortClass.valueOf(substring[9]),
+                            Integer.parseInt(substring[10]),
+                            Boolean.parseBoolean(substring[11]))
+                    );
+                    break;
+                case BUSINESS_CAR:
+                    fileCars.add(new BusinessCar(
+                            substring[1],
+                            substring[2],
+                            Integer.parseInt(substring[3]),
+                            Double.parseDouble(substring[4]),
+                            Double.parseDouble(substring[5]),
+                            Double.parseDouble(substring[6]),
+                            Double.parseDouble(substring[7]),
+                            Integer.parseInt(substring[8]),
+                            BusinessClass.valueOf(substring[9]),
+                            Boolean.parseBoolean(substring[10]),
+                            Integer.parseInt(substring[11]),
+                            Boolean.parseBoolean(substring[12]))
+                    );
+                    break;
+                default: throw new IOException("Wrong value");
+            }
         }
+
+        System.out.println(fileCars);
+
 
 
     }
